99e135b4914892700144c9f52d0de701886d1682
Sean Berndlmaier <138079299+seanb1526@users.noreply.github.com>
2025-05-09 14:06:32 -0400

    Hosted Site on custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/IndoorNavPro.com/', // Match your GitHub repo name exactly
+  base: '/', // Match your GitHub repo name exactly
   build: {
     outDir: 'dist',
   },
