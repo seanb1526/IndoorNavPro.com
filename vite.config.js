bda537b971cfcb6200fac63dffc9c7fba8cbe74c
Sean Berndlmaier <138079299+seanb1526@users.noreply.github.com>
2025-05-09 12:52:46 -0400

    Youtube Tutorial Changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths instead of absolute paths
+  base: '/IndoorNavPro.com', // Use relative paths instead of absolute paths
   build: {
     outDir: 'dist',
   },
